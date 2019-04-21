ea8c51ab71560239726e35b87f597ff76519c648
Piotr Gankiewicz <piotr.gankiewicz@gmail.com>
2019-04-21 17:54:20 +0200

    API Gateway

---
+++ src/Pacco.APIGateway/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Ntrada;
+using Ntrada.Extensions.RabbitMq;
+
+namespace Pacco.APIGateway
+{
+    public static class Program
+    {
+        public static async Task Main(string[] args)
+            => await WebHost.CreateDefaultBuilder(args)
+                .UseNtrada()
+                .UseRabbitMq()
+                .Build()
+                .RunAsync();
+    }
+}
