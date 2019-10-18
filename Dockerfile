524ea1b1bd8facfdd618f2792e82a7743aff5227
Piotr Gankiewicz <piotr.gankiewicz@gmail.com>
2019-10-18 22:28:04 +0200

    Core 3.0 update

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,11 @@
-FROM mcr.microsoft.com/dotnet/core/sdk:2.2 AS build
+FROM mcr.microsoft.com/dotnet/core/sdk:3.0 AS build
 WORKDIR /app
 COPY . .
 RUN dotnet publish src/Pacco.APIGateway -c release -o out
 
-FROM mcr.microsoft.com/dotnet/core/aspnet:2.2
+FROM mcr.microsoft.com/dotnet/core/aspnet:3.0
 WORKDIR /app
-COPY --from=build /app/src/Pacco.APIGateway/out .
+COPY --from=build /app/out .
 ENV ASPNETCORE_URLS http://*:5000
 ENV ASPNETCORE_ENVIRONMENT docker
 ENV NTRADA_CONFIG ntrada.docker
--- src/Pacco.APIGateway/Program.cs
+++ src/Pacco.APIGateway/Program.cs
@@ -4,10 +4,10 @@ using System.Threading.Tasks;
 using Convey;
 using Convey.Logging;
 using Convey.Metrics.AppMetrics;
-using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using Ntrada;
 using Ntrada.Extensions.RabbitMq;
 using Ntrada.Hooks;
@@ -17,23 +17,27 @@ namespace Pacco.APIGateway
 {
     public static class Program
     {
-        public static async Task Main(string[] args)
-            => await WebHost.CreateDefaultBuilder(args)
-                .ConfigureAppConfiguration(builder =>
+        public static Task Main(string[] args)
+            => CreateHostBuilder(args).Build().RunAsync();
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
                 {
-                    var ntradaConfig = Environment.GetEnvironmentVariable("NTRADA_CONFIG");
-                    var configPath = args?.FirstOrDefault() ?? ntradaConfig ?? "ntrada.yml";
-                    builder.AddYamlFile(configPath, false);
-                })
-                .ConfigureServices(services => services.AddNtrada()
-                    .AddOpenTracing()
-                    .AddSingleton<IContextBuilder, CorrelationContextBuilder>()
-                    .AddSingleton<IBeforeHttpClientRequestHook, CorrelationContextHttpHook>()
-                    .AddConvey()
-                    .AddMetrics())
-                .Configure(app => app.UseNtrada())
-                .UseLogging()
-                .Build()
-                .RunAsync();
+                    webBuilder.ConfigureAppConfiguration(builder =>
+                        {
+                            var ntradaConfig = Environment.GetEnvironmentVariable("NTRADA_CONFIG");
+                            var configPath = args?.FirstOrDefault() ?? ntradaConfig ?? "ntrada.yml";
+                            builder.AddYamlFile(configPath, false);
+                        })
+                        .ConfigureServices(services => services.AddNtrada()
+                            .AddOpenTracing()
+                            .AddSingleton<IContextBuilder, CorrelationContextBuilder>()
+                            .AddSingleton<IBeforeHttpClientRequestHook, CorrelationContextHttpHook>()
+                            .AddConvey()
+                            .AddMetrics())
+                        .Configure(app => app.UseNtrada())
+                        .UseLogging();
+                });
     }
 }
